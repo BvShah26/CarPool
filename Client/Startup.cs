cc319cbba4aeec46a6d23308b0682cfab57909a5
Bhavya Shah <bhavyashah.dcs22n@vnsgu.ac.in>
2022-03-27 23:52:00 +0530

    Admin Layout Added

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -51,6 +51,9 @@ namespace Client
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
