7f2ebead16d939334dcd513a5f1e74e818fb83fa
Bhavya Shah <bhavyashah.dcs22n@vnsgu.ac.in>
2022-03-28 10:09:48 +0530

    Api Vehicle Type Started

---
+++ Apis/VehicleTypesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Apis.Data;
+using DataAcessLayer.Models.VehicleModels;
+
+namespace Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleTypesController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public VehicleTypesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VehicleTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VehicleType>>> GetVehicleTypes()
+        {
+            return await _context.VehicleTypes.ToListAsync();
+        }
+
+        // GET: api/VehicleTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VehicleType>> GetVehicleType(int id)
+        {
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return vehicleType;
+        }
+
+        // PUT: api/VehicleTypes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehicleType(int id, VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehicleType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehicleTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/VehicleTypes
+        
+        [HttpPost]
+        public async Task<ActionResult<VehicleType>> PostVehicleType(VehicleType vehicleType)
+        {
+            _context.VehicleTypes.Add(vehicleType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehicleType", new { id = vehicleType.Id }, vehicleType);
+        }
+
+        // DELETE: api/VehicleTypes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<VehicleType>> DeleteVehicleType(int id)
+        {
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            _context.VehicleTypes.Remove(vehicleType);
+            await _context.SaveChangesAsync();
+
+            return vehicleType;
+        }
+
+        private bool VehicleTypeExists(int id)
+        {
+            return _context.VehicleTypes.Any(e => e.Id == id);
+        }
+    }
+}
